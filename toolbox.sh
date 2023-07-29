7b7972d183cd88bdb116df6a537045328cf02e08
Takumi123x <takurukagami12@outlook.com>
2023-07-29 18:10:54 +0700

    Update toolbox.sh

--- toolbox.sh
+++ toolbox.sh
@@ -303,7 +303,7 @@ clear
      M1(){ TPUT 18 $MU_X; $e "A03 core                        ";$ff;}
      M2(){ TPUT 20 $MU_X; $e "A01 core                        ";$ff;}
      M3(){ TPUT 22 $MU_X; $e "Exit                            ";$ff;}
-      LM=3
+      LM=2
    MENU(){ for each in $(seq 0 $LM);do M${each};done;}
     POS(){ if [[ $cur == up ]];then ((i--));fi
            if [[ $cur == dn ]];then ((i++));fi
