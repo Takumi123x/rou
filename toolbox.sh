ef3fd76a711305655a9a200d6e8d485f6d20f113
Takumi123x <takurukagami12@outlook.com>
2023-07-29 11:40:46 +0700

    Update toolbox.sh

--- toolbox.sh
+++ toolbox.sh
@@ -30,10 +30,83 @@ ______________________
  | |
  | |
  ||||||||||||||||||||||"
+ 
+Build_done="
+______________________
+ | |Super image kitchen GUI
+ | |> Build Done
+ | |
+ | |
+ | |
+ | |
+ | |
+ | |
+ | |
+ ||||||||||||||||||||||"
+ 
+clear_kitchen="
+______________________
+ | |Super image kitchen GUI
+ | |> Clear temp folder done
+ | |
+ | |
+ | |
+ | |
+ | |
+ | |
+ | |
+ ||||||||||||||||||||||"
 
 Build_rom (){
 if [ -e ~/kitchen-tmp/super_map.txt ]
 then
+build_done (){
+clear
+      E='echo -e';e='echo -en';trap "R;exit" 2
+    ESC=$( $e "\e")
+   TPUT(){ $e "\e[${1};${2}H";}
+  CLEAR(){ $e "\ec";}
+  CIVIS(){ $e "\e[?25l";}
+   DRAW(){ $e "\e%@\e(0";}
+  WRITE(){ $e "\e(B";}
+      R(){ CLEAR ;stty sane;$e "\ec\e[37;00m\e[J";};
+   HEAD(){ DRAW
+           for each in $(seq 45 25);do
+           $E "   x                                          x"
+           done
+           WRITE;MARK;TPUT 1 1
+           $E " ";UNMARK;}
+           i=0; CLEAR; CIVIS;NULL=/dev/null
+   FOOT(){ MARK;TPUT 47 2
+           printf "ENTER - SELECT,NEXT                     ";TPUT  2 2; $e "Kitchen Tools Termux GUI Test           ";}
+   FOOT2(){ UNMARK;TPUT 3 45
+   printf "$set_info";}
+  ARROW(){ read -s -n3 key 2>/dev/null >&2
+           if [[ $key = $ESC[A ]];then echo up;fi
+           if [[ $key = $ESC[B ]];then echo dn;fi;}
+     M0(){ TPUT 16 $MU_X; $e "Back to Build menu              ";$ff;}
+     M1(){ TPUT 18 $MU_X; $e "Back to Main menu               ";$ff;}
+      LM=1
+   MENU(){ for each in $(seq 0 $LM);do M${each};done;}
+    POS(){ if [[ $cur == up ]];then ((i--));fi
+           if [[ $cur == dn ]];then ((i++));fi
+           if [[ $i -lt 0   ]];then i=$LM;fi
+           if [[ $i -gt $LM ]];then i=0;fi;}
+REFRESH(){ after=$((i+1)); before=$((i-1))
+           if [[ $before -lt 0  ]];then before=$LM;fi
+           if [[ $after -gt $LM ]];then after=0;fi
+           if [[ $j -lt $i      ]];then UNMARK;M$before;else UNMARK;M$after;fi
+           if [[ $after -eq 0 ]] || [ $before -eq $LM ];then
+           UNMARK; M$before; M$after;fi;j=$i;UNMARK;M$before;M$after;}
+   INIT(){ clear;set_info=$build_done;R;HEAD;FOOT2;FOOT;MENU;}
+     SC(){ REFRESH;MARK;$S;$b;cur=`ARROW`;}
+   ES(){ MARK;$e "ENTER = main menu ";$b;read;INIT;};INIT
+  while [[ "$O" != " " ]]; do case $i in
+        0) S=M0;SC;if [[ $cur == "" ]];then R;clear;Build_rom;INIT;fi;;
+        1) S=M1;SC;if [[ $cur == "" ]];then R;clear;main_main;INIT;fi;;
+ esac;POS;done
+}
+
 build_now (){
 if [ -e ~/storage/shared/build-kitchen/odm.img ]
 then
@@ -55,6 +128,8 @@ lpmake --metadata-size 65536 --super-name super --metadata-slots 2 --device supe
 fi
 
 tar -cvf ~/storage/shared/build-kitchen/super.tar ~/storage/shared/build-kitchen/super.img
+rm -rf ~/storage/shared/build-kitchen/super.img
+build_done
 }
 
 build_lz4 (){
@@ -79,7 +154,10 @@ lpmake --metadata-size 65536 --super-name super --metadata-slots 2 --device supe
 fi
 
 lz4 ~/storage/shared/build-kitchen/super.img
-tar -cvf ~/storage/shared/build-kitchen/super.tar ~/storage/shared/build-kitchen/super.lz4
+tar -cvf ~/storage/shared/build-kitchen/super.tar ~/storage/shared/build-kitchen/super.img.lz4
+rm -rf ~/storage/shared/build-kitchen/super.img.lz4
+rm -rf ~/storage/shared/build-kitchen/super.img
+build_done
 }
 
 build_xz (){
@@ -103,6 +181,8 @@ lpmake --metadata-size 65536 --super-name super --metadata-slots 2 --device supe
 fi
 
 tar --xz -cvf ~/storage/shared/build-kitchen/super.tar.xz ~/storage/shared/build-kitchen/super.img
+rm -rf ~/storage/shared/build-kitchen/super.img
+build_done
 }
 
  
@@ -157,8 +237,8 @@ REFRESH(){ after=$((i+1)); before=$((i-1))
 else
 a03s_profile (){
 fileds=a03s.txt
-cp -rf ~/rou/fake/profile/$fileds.txt ~/kitchen-tmp/
-mv -rf ~/kitchen-tmp/$fileds.txt ~/kitchen-tmp/super_map.txt
+cp -rf ~/rou/fake/profile/$fileds ~/kitchen-tmp/
+mv -f ~/kitchen-tmp/$fileds ~/kitchen-tmp/super_map.txt
 printf "$(<~/kitchen-tmp/super_map.txt)" | grep -e "Size:" | awk '{print $2}' > ~/kitchen-tmp/super.txt
 printf "$(<~/kitchen-tmp/super_map.txt)" | grep -e "Maximum size:" | awk '{print $3}' | sed '2!d' > ~/kitchen-tmp/main.txt
 Build_rom
@@ -166,15 +246,11 @@ Build_rom
 
 a01core_profile (){
 fileds=a01core.txt
-cp -rf ~/rou/fake/profile/$fileds.txt ~/kitchen-tmp/
-mv -rf ~/kitchen-tmp/$fileds.txt ~/kitchen-tmp/super_map.txt
+cp -rf ~/rou/fake/profile/$fileds ~/kitchen-tmp/
+mv -f ~/kitchen-tmp/$fileds ~/kitchen-tmp/super_map.txt
 printf "$(<~/kitchen-tmp/super_map.txt)" | grep -e "Size:" | awk '{print $2}' > ~/kitchen-tmp/super.txt
 printf "$(<~/kitchen-tmp/super_map.txt)" | grep -e "Maximum size:" | awk '{print $3}' | sed '2!d' > ~/kitchen-tmp/main.txt
 Build_rom
-}
-
-a03_profile (){
-
 }
 
 #custom profile
@@ -203,8 +279,7 @@ clear
            if [[ $key = $ESC[B ]];then echo dn;fi;}
      M0(){ TPUT 16 $MU_X; $e "A03S                            ";$ff;}
      M1(){ TPUT 18 $MU_X; $e "A01 core                        ";$ff;}
-     M2(){ TPUT 20 $MU_X; $e "None                            ";$ff;}
-     M3(){ TPUT 22 $MU_X; $e "exit                            ";$ff;}
+     M2(){ TPUT 20 $MU_X; $e "Exit                            ";$ff;}
       LM=3
    MENU(){ for each in $(seq 0 $LM);do M${each};done;}
     POS(){ if [[ $cur == up ]];then ((i--));fi
@@ -224,7 +299,6 @@ REFRESH(){ after=$((i+1)); before=$((i-1))
         0) S=M0;SC;if [[ $cur == "" ]];then R;clear;a03s_profile;INIT;fi;;
         1) S=M1;SC;if [[ $cur == "" ]];then R;clear;a01core_profile;INIT;fi;;
         2) S=M2;SC;if [[ $cur == "" ]];then R;clear;main_main;INIT;fi;;
-        3) S=M3;SC;if [[ $cur == "" ]];then R;clear;main_main;fi;;
  esac;POS;done
 fi
 }
@@ -233,7 +307,7 @@ Extract_rom (){
 extract_internal (){
 if [ 1 == "$(find ~/storage/shared/build-kitchen/$filesuper -type f ! -size 0 -printf '%S\n' | sed 's/\.[0-9]*//')"]
 then
-mv -rf ~/storage/shared/build-kitchen/$filesuper ~/storage/shared/build-kitchen/$filesupersp
+mv -f ~/storage/shared/build-kitchen/$filesuper ~/storage/shared/build-kitchen/$filesupersp
 else
 simg2img ~/storage/shared/build-kitchen/$filesuper ~/storage/shared/build-kitchen/$filesupersp
 fi
@@ -283,8 +357,7 @@ clear
            if [[ $key = $ESC[B ]];then echo dn;fi;}
      M0(){ TPUT 16 $MU_X; $e "Extract from current phone(root)";$ff;}
      M1(){ TPUT 18 $MU_X; $e "Extract from internal           ";$ff;}
-     M2(){ TPUT 20 $MU_X; $e "About                           ";$ff;}
-     M3(){ TPUT 22 $MU_X; $e "exit                            ";$ff;}
+     M2(){ TPUT 20 $MU_X; $e "Exit                            ";$ff;}
       LM=3
    MENU(){ for each in $(seq 0 $LM);do M${each};done;}
     POS(){ if [[ $cur == up ]];then ((i--));fi
@@ -303,13 +376,57 @@ REFRESH(){ after=$((i+1)); before=$((i-1))
   while [[ "$O" != " " ]]; do case $i in
         0) S=M0;SC;if [[ $cur == "" ]];then R;clear;extract_root;INIT;fi;;
         1) S=M1;SC;if [[ $cur == "" ]];then R;clear;extract_internal;INIT;fi;;
-        2) S=M2;SC;if [[ $cur == "" ]];then R;clear;About_GMSENADIS;INIT;fi;;
-        3) S=M3;SC;if [[ $cur == "" ]];then R;clear;main_main;fi;;
+        2) S=M2;SC;if [[ $cur == "" ]];then R;clear;main_main;fi;;
  esac;POS;done
 }
 
 clear_kitchen (){
 rm ~/kitchen-tmp
+
+clear
+      E='echo -e';e='echo -en';trap "R;exit" 2
+    ESC=$( $e "\e")
+   TPUT(){ $e "\e[${1};${2}H";}
+  CLEAR(){ $e "\ec";}
+  CIVIS(){ $e "\e[?25l";}
+   DRAW(){ $e "\e%@\e(0";}
+  WRITE(){ $e "\e(B";}
+      R(){ CLEAR ;stty sane;$e "\ec\e[37;00m\e[J";};
+   HEAD(){ DRAW
+           for each in $(seq 45 25);do
+           $E "   x                                          x"
+           done
+           WRITE;MARK;TPUT 1 1
+           $E " ";UNMARK;}
+           i=0; CLEAR; CIVIS;NULL=/dev/null
+   FOOT(){ MARK;TPUT 47 2
+           printf "ENTER - SELECT,NEXT                     ";TPUT  2 2; $e "Kitchen Tools Termux GUI Test           ";}
+   FOOT2(){ UNMARK;TPUT 3 45
+   printf "$set_info";}
+  ARROW(){ read -s -n3 key 2>/dev/null >&2
+           if [[ $key = $ESC[A ]];then echo up;fi
+           if [[ $key = $ESC[B ]];then echo dn;fi;}
+     M0(){ TPUT 16 $MU_X; $e "Back to Build menu              ";$ff;}
+     M1(){ TPUT 18 $MU_X; $e "Back to Main menu               ";$ff;}
+      LM=1
+   MENU(){ for each in $(seq 0 $LM);do M${each};done;}
+    POS(){ if [[ $cur == up ]];then ((i--));fi
+           if [[ $cur == dn ]];then ((i++));fi
+           if [[ $i -lt 0   ]];then i=$LM;fi
+           if [[ $i -gt $LM ]];then i=0;fi;}
+REFRESH(){ after=$((i+1)); before=$((i-1))
+           if [[ $before -lt 0  ]];then before=$LM;fi
+           if [[ $after -gt $LM ]];then after=0;fi
+           if [[ $j -lt $i      ]];then UNMARK;M$before;else UNMARK;M$after;fi
+           if [[ $after -eq 0 ]] || [ $before -eq $LM ];then
+           UNMARK; M$before; M$after;fi;j=$i;UNMARK;M$before;M$after;}
+   INIT(){ clear;set_info=$clear_kitchen;R;HEAD;FOOT2;FOOT;MENU;}
+     SC(){ REFRESH;MARK;$S;$b;cur=`ARROW`;}
+   ES(){ MARK;$e "ENTER = main menu ";$b;read;INIT;};INIT
+  while [[ "$O" != " " ]]; do case $i in
+        0) S=M0;SC;if [[ $cur == "" ]];then R;clear;Build_rom;INIT;fi;;
+        1) S=M1;SC;if [[ $cur == "" ]];then R;clear;main_main;INIT;fi;;
+ esac;POS;done
 }
 
 main_main (){
@@ -348,12 +465,10 @@ MU_X=5
            if [[ $key = $ESC[A ]];then echo up;fi
            if [[ $key = $ESC[B ]];then echo dn;fi;}
      M0(){ TPUT 16 $MU_X; $e "Extract Super                   ";$ff;}
-     M1(){ TPUT 18 $MU_X; $e "Mount partition                 ";$ff;}
-     M2(){ TPUT 20 $MU_X; $e "Build ROM                       ";$ff;}
-     M3(){ TPUT 22 $MU_X; $e "Clear Temp folder               ";$ff;}
-     M4(){ TPUT 24 $MU_X; $e "About                           ";$ff;}
-     M5(){ TPUT 26 $MU_X; $e "exit                            ";$ff;}
-      LM=5
+     M1(){ TPUT 18 $MU_X; $e "Build ROM                       ";$ff;}
+     M2(){ TPUT 20 $MU_X; $e "Clear Temp folder               ";$ff;}
+     M3(){ TPUT 22 $MU_X; $e "exit                            ";$ff;}
+      LM=3
    MENU(){ for each in $(seq 0 $LM);do M${each};done;}
     POS(){ if [[ $cur == up ]];then ((i--));fi
            if [[ $cur == dn ]];then ((i++));fi
@@ -370,11 +485,9 @@ REFRESH(){ after=$((i+1)); before=$((i-1))
    ES(){ MARK;$e "ENTER = main menu ";$b;read;INIT;};INIT
   while [[ "$O" != " " ]]; do case $i in
         0) S=M0;SC;if [[ $cur == "" ]];then R;clear;Extract_rom;INIT;fi;;
-        1) S=M1;SC;if [[ $cur == "" ]];then R;clear;Mount_rom;INIT;fi;;
-        2) S=M2;SC;if [[ $cur == "" ]];then R;clear;Build_rom;INIT;fi;;
-        3) S=M3;SC;if [[ $cur == "" ]];then R;clear;clear_kitchen;fi;;
-        4) S=M4;SC;if [[ $cur == "" ]];then R;clear;Info_p1;fi;;
-        5) S=M5;SC;if [[ $cur == "" ]];then R;clear;exit;fi;;
+        1) S=M1;SC;if [[ $cur == "" ]];then R;clear;Build_rom;INIT;fi;;
+        2) S=M2;SC;if [[ $cur == "" ]];then R;clear;clear_kitchen;fi;;
+        3) S=M3;SC;if [[ $cur == "" ]];then R;clear;exit;fi;;
  esac;POS;done
 }
 
