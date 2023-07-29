96c100c789f894a33d270ef99b9a2da5f8a046da
Takumi123x <takurukagami12@outlook.com>
2023-07-29 10:48:23 +0700

    Update profile option

--- toolbox.sh
+++ toolbox.sh
@@ -32,6 +32,8 @@ ______________________
  ||||||||||||||||||||||"
 
 Build_rom (){
+if [ -e ~/kitchen-tmp/super_map.txt ]
+then
 build_now (){
 if [ -e ~/storage/shared/build-kitchen/odm.img ]
 then
@@ -103,7 +105,7 @@ fi
 tar --xz -cvf ~/storage/shared/build-kitchen/super.tar.xz ~/storage/shared/build-kitchen/super.img
 }
 
-#Device info
+ 
 clear
       E='echo -e';e='echo -en';trap "R;exit" 2
     ESC=$( $e "\e")
@@ -148,10 +150,83 @@ REFRESH(){ after=$((i+1)); before=$((i-1))
    ES(){ MARK;$e "ENTER = main menu ";$b;read;INIT;};INIT
   while [[ "$O" != " " ]]; do case $i in
         0) S=M0;SC;if [[ $cur == "" ]];then R;clear;build_now;INIT;fi;;
-        1) S=M1;SC;if [[ $cur == "" ]];then R;clear;extract_internal;INIT;fi;;
-        2) S=M2;SC;if [[ $cur == "" ]];then R;clear;About_GMSENADIS;INIT;fi;;
+        1) S=M1;SC;if [[ $cur == "" ]];then R;clear;build_lz4;INIT;fi;;
+        2) S=M2;SC;if [[ $cur == "" ]];then R;clear;build_xz;INIT;fi;;
         3) S=M3;SC;if [[ $cur == "" ]];then R;clear;main_main;fi;;
  esac;POS;done
+else
+a03s_profile (){
+fileds=a03s.txt
+cp -rf ~/rou/fake/profile/$fileds.txt ~/kitchen-tmp/
+mv -rf ~/kitchen-tmp/$fileds.txt ~/kitchen-tmp/super_map.txt
+printf "$(<~/kitchen-tmp/super_map.txt)" | grep -e "Size:" | awk '{print $2}' > ~/kitchen-tmp/super.txt
+printf "$(<~/kitchen-tmp/super_map.txt)" | grep -e "Maximum size:" | awk '{print $3}' | sed '2!d' > ~/kitchen-tmp/main.txt
+Build_rom
+}
+
+a01core_profile (){
+fileds=a01core.txt
+cp -rf ~/rou/fake/profile/$fileds.txt ~/kitchen-tmp/
+mv -rf ~/kitchen-tmp/$fileds.txt ~/kitchen-tmp/super_map.txt
+printf "$(<~/kitchen-tmp/super_map.txt)" | grep -e "Size:" | awk '{print $2}' > ~/kitchen-tmp/super.txt
+printf "$(<~/kitchen-tmp/super_map.txt)" | grep -e "Maximum size:" | awk '{print $3}' | sed '2!d' > ~/kitchen-tmp/main.txt
+Build_rom
+}
+
+a03_profile (){
+
+}
+
+#custom profile
+clear
+      E='echo -e';e='echo -en';trap "R;exit" 2
+    ESC=$( $e "\e")
+   TPUT(){ $e "\e[${1};${2}H";}
+  CLEAR(){ $e "\ec";}
+  CIVIS(){ $e "\e[?25l";}
+   DRAW(){ $e "\e%@\e(0";}
+  WRITE(){ $e "\e(B";}
+      R(){ CLEAR ;stty sane;$e "\ec\e[37;00m\e[J";};
+   HEAD(){ DRAW
+           for each in $(seq 45 25);do
+           $E "   x                                          x"
+           done
+           WRITE;MARK;TPUT 1 1
+           $E " ";UNMARK;}
+           i=0; CLEAR; CIVIS;NULL=/dev/null
+   FOOT(){ MARK;TPUT 47 2
+           printf "ENTER - SELECT,NEXT                     ";TPUT  2 2; $e "Kitchen Tools Termux GUI Test           ";}
+   FOOT2(){ UNMARK;TPUT 3 45
+   printf "$set_info";}
+  ARROW(){ read -s -n3 key 2>/dev/null >&2
+           if [[ $key = $ESC[A ]];then echo up;fi
+           if [[ $key = $ESC[B ]];then echo dn;fi;}
+     M0(){ TPUT 16 $MU_X; $e "A03S                            ";$ff;}
+     M1(){ TPUT 18 $MU_X; $e "A01 core                        ";$ff;}
+     M2(){ TPUT 20 $MU_X; $e "None                            ";$ff;}
+     M3(){ TPUT 22 $MU_X; $e "exit                            ";$ff;}
+      LM=3
+   MENU(){ for each in $(seq 0 $LM);do M${each};done;}
+    POS(){ if [[ $cur == up ]];then ((i--));fi
+           if [[ $cur == dn ]];then ((i++));fi
+           if [[ $i -lt 0   ]];then i=$LM;fi
+           if [[ $i -gt $LM ]];then i=0;fi;}
+REFRESH(){ after=$((i+1)); before=$((i-1))
+           if [[ $before -lt 0  ]];then before=$LM;fi
+           if [[ $after -gt $LM ]];then after=0;fi
+           if [[ $j -lt $i      ]];then UNMARK;M$before;else UNMARK;M$after;fi
+           if [[ $after -eq 0 ]] || [ $before -eq $LM ];then
+           UNMARK; M$before; M$after;fi;j=$i;UNMARK;M$before;M$after;}
+   INIT(){ clear;set_info=$super_info;R;HEAD;FOOT2;FOOT;MENU;}
+     SC(){ REFRESH;MARK;$S;$b;cur=`ARROW`;}
+   ES(){ MARK;$e "ENTER = main menu ";$b;read;INIT;};INIT
+  while [[ "$O" != " " ]]; do case $i in
+        0) S=M0;SC;if [[ $cur == "" ]];then R;clear;a03s_profile;INIT;fi;;
+        1) S=M1;SC;if [[ $cur == "" ]];then R;clear;a01core_profile;INIT;fi;;
+        2) S=M2;SC;if [[ $cur == "" ]];then R;clear;main_main;INIT;fi;;
+        3) S=M3;SC;if [[ $cur == "" ]];then R;clear;main_main;fi;;
+ esac;POS;done
+fi
 }
 
 Extract_rom (){
@@ -182,7 +257,7 @@ fi
 
 filesuper=super.img
 filesupersp=super_raw.img
-#Device info
+ 
 clear
       E='echo -e';e='echo -en';trap "R;exit" 2
     ESC=$( $e "\e")
@@ -245,7 +320,7 @@ main_tmp=~/kitchen-tmp
 mkdir ~/kitchen-tmp
 ln -s ~/storage/shared/build-kitchen ~/
 clear
-#Device info
+ 
 clear
 MU_X=5
       E='echo -e';e='echo -en';trap "R;exit" 2
